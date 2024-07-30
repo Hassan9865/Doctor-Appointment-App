fb64de0366332314f4e1e1e4038b9668c2c8a7ea
Hassan <syedhassanraza0311@gmail.com>
2024-07-31 00:01:30 +0500

    __

--- lib/view/Home view/widgets/find_doctor.dart
+++ lib/view/Home view/widgets/find_doctor.dart
@@ -1,32 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class FindDoctor extends StatelessWidget {
-//   final String? text;
-//   final void Function()? ontap;
-//   final AssetImage? assetimage;
-//   const FindDoctor({super.key, this.text, this.assetimage, this.ontap});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.all(18.0),
-//       child: Column(
-//         children: [
-//           GestureDetector(
-//             onTap: ontap,
-//             child: CircleAvatar(
-//               radius: MediaQuery.of(context).size.width / 12,
-//               backgroundColor: Colors.grey[300],
-//               backgroundImage:
-//                   assetimage ?? AssetImage('assets\images\pic5.png'),
-//             ),
-//           ),
-//           Text(text ?? ""),
-//         ],
-//       ),
-//     );
-//   }
-// }
 import 'package:doctor_appoint_app/view/Home%20view/Home_viewModel.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
